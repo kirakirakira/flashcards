9bdea4cb3637ccb5d153d321d4e13aed61fc7ab0
Kira Hartlage <kira.hartlage@gmail.com>
2019-06-07 14:36:50 -0400

    can PUT to myjson

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 
+import AddTermForm from './Components/AddTermForm';
 import TermList from './Components/TermList';
 
 class App extends Component {
@@ -25,12 +26,30 @@ class App extends Component {
          })
   }
 
+  addTerm = (term) => {
+    const { terms } = this.state;
+
+    terms.push(term);
+
+    axios.put('https://api.myjson.com/bins/125clh',
+          { "terms": terms })
+         .then(response => {
+           this.setState({
+             terms: response.data.terms
+           })
+         })
+         .catch(error => {
+           console.log('Error putting and fetching data', error);
+         })
+  }
+
   render() {
     console.log(this.state.terms);
     return (
       <div>
         <h1>Flashcard App!</h1>
         <TermList data={this.state.terms} />
+        <AddTermForm addTerm={this.addTerm.bind(this)} />
       </div>
     )
   }
---
+++ src/Components/AddTermForm.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class AddTermForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      termInput: ''
+    }
+  }
+
+  render() {
+    return (
+      <form onSubmit={ (event) => {
+        event.preventDefault();
+        this.props.addTerm(this.state.termInput);
+        this.setState({termInput: ''})
+      }}>
+        <label>Term: </label>
+        <input
+          type="text"
+          id="term"
+          value={this.state.termInput}
+          onChange={ (event) => {
+            this.setState({
+              termInput: event.target.value
+            })
+          }}
+        />
+        <input type="submit" />
+      </form>
+    )
+  }
+}
+
+export default AddTermForm;
