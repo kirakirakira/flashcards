39be3be54d9651f9991a99cbf5a35ce19cecbabf
Kira Hartlage <kira.hartlage@gmail.com>
2019-06-27 19:21:49 -0400

    removed tabs from testall and matched styling of testrandom

--- src/Components/TestAll.js
+++ src/Components/TestAll.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Row, Tabs, Tab } from 'react-bootstrap';
+import { Button, Container, Row, Tabs, Tab } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 class TestAll extends Component {
@@ -38,25 +38,19 @@ class TestAll extends Component {
   render() {
     return (
       <Container>
-        <Tabs
-          activeKey={this.state.key}
-          id="uncontrolled-tab-example"
-          onSelect={key => this.setState({ key })}
-          className="cardTabs"
-        >
-          <Tab eventKey="term" title="Term">
-            <p className="term">{this.state.cards[this.state.currentIndex].term}</p>
-          </Tab>
-          <Tab eventKey="definition" title="Definition">
-            <p className="definition">{this.state.cards[this.state.currentIndex].definition}</p>
-          </Tab>
-        </Tabs>
-
-        <div className="nextButton">
-          <button onClick={this.seeNext}>Next</button>
+        <div id="testRandomBlock">
+          Term: {this.state.cards[this.state.currentIndex].term}
+          {this.state.showDefinition ?
+            <div>
+              <p className="definition">Definition: {this.state.cards[this.state.currentIndex].definition}</p>
+            </div> :
+            null
+          }
+          <div className="testButtons">
+            <Button onClick={this.seeDefinition} id="seeDefButton">See Definition</Button>
+            <Button onClick={this.seeNext}>Next</Button>
+          </div>
         </div>
-
-        <Link to="/"><button>Go back home</button></Link>
       </Container>
     )
   }
